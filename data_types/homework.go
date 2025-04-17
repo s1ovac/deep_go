96feac6ae716246b00ba253cd28499742f1f4d4f
Ilya Pereverzev <pereverzev.ilya2@wb.ru>
2025-04-17 14:13:58 +0300

    Added LittleEndian function to convert numbers.

--- data_types/homework.go
+++ data_types/homework.go
@@ -3,5 +3,8 @@ package data_types
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+	return ((number >> 24) & 0xff) |
+		((number >> 8) & 0xff00) |
+		((number << 8) & 0xff0000) |
+		((number << 24) & 0xff000000)
 }
