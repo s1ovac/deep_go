17fa6340ac50c77d0afa2bc14bea2f635747e528
Ilya Pereverzev <pereverzev.ilya2@wb.ru>
2025-04-20 23:21:55 +0300

    Added logic to methods.

--- strings/homework.go
+++ strings/homework.go
@@ -1,27 +1,60 @@
 package strings
 
+import "unsafe"
+
 type COWBuffer struct {
 	data []byte
 	refs *int
-	// need to implement
 }
 
 func NewCOWBuffer(data []byte) COWBuffer {
-	return COWBuffer{} // need to implement
+	return COWBuffer{
+		data: data,
+		refs: new(int),
+	}
 }
 
+// Clone - создать новую копию буфера.
+//
+// Предположим, что все будут производить копирование
+// буффера только с использованием метода Clone()
 func (b *COWBuffer) Clone() COWBuffer {
-	return COWBuffer{} // need to implement
+	if b.refs != nil {
+		*b.refs++
+	}
+
+	return COWBuffer{
+		data: b.data,
+	}
 }
 
+// Close - перестать использовать копию буффера.
 func (b *COWBuffer) Close() {
-	// need to implement
+	if b.refs != nil && *b.refs > 0 {
+		*b.refs--
+	}
 }
 
+// Update - изменить определенный байт в буффере.
 func (b *COWBuffer) Update(index int, value byte) bool {
-	return false // need to implement
+	if len(b.data) == 0 || index < 0 || index >= len(b.data) {
+		return false
+	}
+
+	if b.refs != nil && *b.refs > 0 {
+		*b.refs--
+
+		newData := make([]byte, len(b.data))
+		copy(newData, b.data)
+		b.data = newData
+	}
+
+	b.data[index] = value
+
+	return true
 }
 
+// String - сконвертировать буффер в строку.
 func (b *COWBuffer) String() string {
-	return "" // need to implement
+	return unsafe.String(unsafe.SliceData(b.data), len(b.data))
 }
